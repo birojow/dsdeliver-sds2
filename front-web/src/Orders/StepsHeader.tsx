5c4d4da5015f53d576c09226b42fd7107d5fa943
Fabiano de Mello <f.augmello@hotmail.com>
2021-01-09 16:33:29 -0300

    finish front-web

--- front-web/src/Orders/StepsHeader.tsx
+++ front-web/src/Orders/StepsHeader.tsx
@@ -1,8 +1,8 @@
 function StepsHeader() {
     return (
         <header className="orders-steps-container">
-            <div className="order-steps-content">
-                <h1 className="stepsTitle">
+            <div className="orders-steps-content">
+                <h1 className="steps-title">
                     SIGA AS <br /> ETAPAS
                 </h1>
                 <ul className="steps-items">
